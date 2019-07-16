df45719072a740b5c797d903e397f61f09575adc
masagrator <masagrator#gmail.com>
2019-07-16 16:31:02 +0200

    clean repo

--- include/lib/lua_bind_hash.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <switch_min.h>
-
-uint64_t lua_bind_hash(const void* data_, size_t len);
-uint64_t lua_bind_hash_str(const char* str);
--- include/useful/raygun_printer.h
+++
@@ -1,3 +0,0 @@
-#include <switch_min.h>
-
-void print_string(u64 module_accessor, const char* format, ...);
